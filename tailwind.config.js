a7c6a14f1c5d73bdac1fb88c12b50a6b5e077a28
savarniknandinee <80510308+savarniknandinee@users.noreply.github.com>
2024-12-08 12:12:21 +0530

    minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,24 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        midnight: '#2C3E50',
-        mystic: '#8E44AD',
-        forest: '#1E824C',
-        enchanted: '#F39C12',
-        smoke: '#BDC3C7',
-        ruby: '#C0392B'
-      },
-      fontFamily: {
-        almendra: ['Almendra', 'serif'],
-        harryp: ['HarryPotter', 'sans-serif']
-      },
-      backdropBlur: {
-        xs: '2px',
-      },
+export const content = [
+  "./src/**/*.{js,jsx,ts,tsx}",
+];
+export const theme = {
+  extend: {
+    colors: {
+      midnight: '#2C3E50',
+      mystic: '#8E44AD',
+      forest: '#1E824C',
+      enchanted: '#F39C12',
+      smoke: '#BDC3C7',
+      ruby: '#C0392B'
+    },
+    fontFamily: {
+      almendra: ['Almendra', 'serif'],
+      harryp: ['HarryPotter', 'sans-serif']
+    },
+    backdropBlur: {
+      xs: '2px',
     },
   },
-  plugins: [],
-}
+};
+export const plugins = [];
